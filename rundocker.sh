2a7a290b7675a38cb63940b696e64f3c7c1a227d
Derrick <gengdeh@gmail.com>
2015-04-18 17:51:04 +0800

    auto rm container

--- rundocker.sh
+++ rundocker.sh
@@ -1,1 +1,1 @@
-docker run -i -t -v /home/workspace/javahome$1:/data/javahome derrickh/demo bash
+docker run --name=$1 -i -t -v /home/workspace/javahome$1:/data/javahome derrickh/demo bash
--- server.js
+++ server.js
@@ -2,9 +2,6 @@ var tty = require('tty.js');
 
 var app = tty.createServer({
   shell: 'bash',
-  users: {
-    //foo: 'bar'
-  },
   port: process.env.PROT
 });
 
